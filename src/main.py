8bfe8c580d28b600264bc441599444b6e5c1ec10
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-26 04:28:03 +0100

    Add CORS middleware and implement BFS algorithm for maze pathfinding

--- src/main.py
+++ src/main.py
@@ -1,24 +1,74 @@
-from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List, Tuple
 
 app = FastAPI()
- 
-import debugpy
-debugpy.listen(("0.0.0.0", 5678))
 
-class Pancake:
-    def __init__(self, cook_time, radius, location):
-        self.cook_time, self.radius, self.location = cook_time, radius, location
-        pass
-    
-    def eu_us(self):
-        if (self.location == "eu"):
-            return "the pancake is from EU"
-        elif (self.location == "us"):
-            return "the pancake is from US"
+# Allow CORS for your frontend (replace '*' with your frontend's URL)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Define maze dimensions
+num_rows = 10
+num_cols = 10
+
+# Sample maze (0 represents open path, 1 represents a wall)
+maze = [
+    [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 0, 1, 1, 0],
+]
+
+# Implement Breadth-First Search (BFS) algorithm
+def bfs_search(start: Tuple[int, int], end: Tuple[int, int]) -> List[Tuple[int, int]]:
+    queue = [(start, [])]
+    visited = set()
+
+    while queue:
+        current, path = queue.pop(0)
+        row, col = current
+
+        if current == end:
+            return path + [current]
+
+        if current not in visited and maze[row][col] == 0:
+            visited.add(current)
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                new_row, new_col = row + dr, col + dc
+                if (
+                    0 <= new_row < num_rows
+                    and 0 <= new_col < num_cols
+                    and (new_row, new_col) not in visited
+                ):
+                    queue.append(((new_row, new_col), path + [current]))
+
+    # If BFS didn't find a path, return an empty list to indicate no path found
+    return []
 
 
-@app.get("/")
-def read_item():
-    food = Pancake(200, 20, "us")
-    return {"food": food.location, "location": food.eu_us(), "cook_time": food.cook_time, "radius": food.radius}
+@app.get("/api/path")
+def get_path():
+    # Define the starting and ending points (modify as needed)
+    start = (0, 0)
+    end = (num_rows - 1, num_cols - 1)
+
+    # Find the path using BFS
+    path = bfs_search(start, end)
+
+    # Check if a path was found
+    if not path:
+        return {"message": "No path found."}
+    else:
+        return {"path": path}
